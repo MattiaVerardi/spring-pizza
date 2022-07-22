0a2fa9c5666355ba3cf343d2e2d7de42345acf60
MattiaVerardi <verardi.mattia97@gmail.com>
2022-07-22 16:09:26 +0200

    consegna base

--- src/main/java/jana60/controller/PizzaController.java
+++ src/main/java/jana60/controller/PizzaController.java
@@ -1,15 +1,21 @@
 package jana60.controller;
 
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jana60.model.Pizza;
 import jana60.repo.PizzaRepo;
@@ -41,5 +47,29 @@ public class PizzaController {
 			repo.save(formPizza);
 			return "redirect:/";
 		}
+	}
+
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer pizzaId, RedirectAttributes ra) {
+		Optional<Pizza> result = repo.findById(pizzaId);
+		if (result.isPresent()) {
+			repo.delete(result.get());
+			ra.addFlashAttribute("successMessage", "La pizza " + result.get().getNome() + " è stata eliminata!");
+			return "redirect:/";
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza con id " + pizzaId + " non è presente");
+		}
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer pizzaId, Model model) {
+		Optional<Pizza> result = repo.findById(pizzaId);
+		if (result.isPresent()) {
+			model.addAttribute("pizza", result.get());
+			return "/pizza/edit";
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza con id " + pizzaId + " non è presente");
+		}
+
 	}
 }
